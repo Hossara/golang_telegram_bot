fef2994f584a1954a28b4e8288f959540a2c0888
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-10-31 12:56:56 +0330

    Implement simple telegram bot

---
+++ go.mod
@@ -0,0 +1,10 @@
+module golang_telegram_bot
+
+go 1.23.2
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.30.0 // indirect
+)
---
+++ simple/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"golang.org/x/net/proxy"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+type ConfigurationType struct {
+	NewUpdateOffset int
+	Timeout         int
+}
+
+var Configuration = &ConfigurationType{
+	NewUpdateOffset: 0,
+	Timeout:         60,
+}
+
+var (
+	client *http.Client
+	bot    *tgbotapi.BotAPI
+)
+
+func main() {
+	// Read .env file
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("error loading .env file: %v", err)
+		return
+	}
+
+	// Set up SOCKS5 proxy
+	if os.Getenv("PROXY") != "" {
+		// Create socks 5 connection
+		dialer, err := proxy.SOCKS5("tcp", os.Getenv("PROXY"), &proxy.Auth{
+			User:     "",
+			Password: "",
+		}, proxy.Direct)
+
+		if err != nil {
+			log.Fatalf("failed to connect to proxy: %v", err)
+			return
+		}
+
+		// Use it in http transport layer
+		transport := &http.Transport{
+			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+				return dialer.Dial(network, addr)
+			},
+		}
+
+		// Create HTTP client with the proxy
+		client = &http.Client{Transport: transport}
+		bot, err = tgbotapi.NewBotAPIWithClient(os.Getenv("TELEGRAM_TOKEN"), client)
+	} else {
+		// Create telegram bot instance without proxy
+		bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	}
+
+	if err != nil {
+		log.Fatalf("Error creating bot: %v", err)
+		return
+	}
+
+	// Set true. the bot to use debug mode (verbose logging).
+	bot.Debug = false
+
+	// Set up updates configuration.
+	u := tgbotapi.NewUpdate(Configuration.NewUpdateOffset)
+	u.Timeout = Configuration.Timeout
+
+	// Get updates from the Telegram API.
+	updates, err := bot.GetUpdatesChan(u)
+
+	if err != nil {
+		log.Fatalf("Error getting updates: %v", err)
+		return
+	}
+
+	for update := range updates {
+		// Ignore any non-Message updates.
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("New message from user @%s", update.Message.From.UserName)
+
+		// Respond to the user.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello! I am your Telegram bot.")
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("Error sending message: %v", err)
+		}
+	}
+}
